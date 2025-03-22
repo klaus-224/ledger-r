bb85cac07903d93ac3abbd8b60327a2c2b18f7a7
R-Ram95 <rohinesh@ualberta.ca>
2025-03-22 12:16:04 -0600

    add sidebar open close animation

--- src/components/window-wrapper.tsx
+++ src/components/window-wrapper.tsx
@@ -1,19 +1,27 @@
-import { ReactNode } from "react";
+import { ReactNode, useState } from "react";
 import SidebarNav from "./sidebar-nav";
 
 const WindowWrapper = ({ children }: { children: ReactNode }) => {
-  return (
-    <div
-      data-slot="window-wrapper"
-      className="min-h-screen flex flex-row"
-    >
-      <div data-slot="side-bar-wrapper" className="min-h-screen border-2 border-red-500 w-1/3">
+	const [sidebarOpen, setSidebarOpen] = useState(true);
+
+	return (
+    <div data-slot="window-wrapper" className="flex flex-row">
+      <div
+        data-slot="side-bar-wrapper"
+        className={`side-bar ${sidebarOpen ? "w-1/4" : "w-1/12"}`}
+      >
         <SidebarNav />
       </div>
-      <div data-slot="content-wrapper" className="min-h-screen w-full border-2 border-blue-500">
+      <div
+        data-slot="content-wrapper"
+        className={`main-content ${sidebarOpen ? "w-3/4" : "w-11/12"}`}
+      >
+				<button onClick={() => setSidebarOpen(!sidebarOpen)}>
+					Toggle Side Bar
+				</button>
         {children}
       </div>
-    </div>
+    </div >
   );
 };
 
