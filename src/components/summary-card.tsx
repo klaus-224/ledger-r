89ab7340fe4de706f44f503b699ed5355a035ac7
R-Ram95 <rohinesh@ualberta.ca>
2025-03-25 20:51:19 -0600

    add main expense page

---
+++ src/components/summary-card.tsx
@@ -0,0 +1,12 @@
+const SummaryCard = () => {
+  return (
+    <div className="card">
+      <div className="p-4">
+        <h3 className="text-lg font-medium text-white mb-2">Expense Title</h3>
+        <p className="text-gray-400">$250.00</p>
+      </div>
+    </div>
+  );
+};
+
+export default SummaryCard;
--- src/panes/expense-pane.tsx
+++ src/panes/expense-pane.tsx
@@ -1,5 +1,22 @@
+import SummaryCard from "@components/summary-card";
+
 const ExpensePane = () => {
-  return <div>Expense Pane</div>;
+  const array = new Array(12).fill(1);
+
+  return (
+    <div className="pane">
+      <div className="flex flex-row justify-between items-center">
+        <h2>Expenses</h2>
+        <button className="button">+ New Expense</button>
+      </div>
+      <div className="flex flex-row flex-wrap justify-between gap-y-5 mt-10">
+        {array.map(() => (
+          <SummaryCard />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default ExpensePane;
+
