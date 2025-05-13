ec7a1e12f02fcaeb5f52d438f1623b8c30bf73b0
R-Ram95 <rohinesh@ualberta.ca>
2025-05-12 20:31:39 -0600

    implemented error handling in react

--- src/lib/utils/utils.ts
+++ src/lib/utils/utils.ts
@@ -1,34 +1,70 @@
+import { AppError } from "@lib/types/models";
 import { invoke } from "@tauri-apps/api/core";
 import { clsx, type ClassValue } from "clsx";
 import { endOfMonth, parseISO } from "date-fns";
 import { twMerge } from "tailwind-merge";
 
+interface Result<T> {
+  result?: { data: T };
+  error?: AppError;
+}
+
 export const getEndOfMonth = (startDate: string) => {
   return endOfMonth(parseISO(startDate));
 };
 
-// TODO: not great usage of TS, improve in the future
+export function checkAppError(e: any) {
+  return e && typeof e === "object" && "kind" in e && "message" in e;
+}
+
 export async function invokeIpc<T>(command: string, params?: any): Promise<T> {
   try {
-    let response: { result: any; error: String };
-
+    let response;
     if (params) {
-      response = await invoke(command, {
+      response = await invoke<Result<T>>(command, {
         params,
       });
     } else {
-      response = await invoke(command);
+      response = await invoke<Result<T>>(command);
     }
 
+    // one of the AppErrors
     if (response.error) {
-      throw new Error(`Command error ${response.error}`);
+      console.error(`Command ${command} failed with error:`, response.error);
+      throw response.error;
     }
 
-    return response.result.data;
-  } catch (e) {
-    console.log("Critical Error:", JSON.stringify(e));
-    throw new Error(`Unknown error in command ${command}`);
+    if (response.result && response.result.data !== undefined) {
+      return response.result.data;
+    }
+
+    // we don't have an error but we also don't have a response
+    console.error(
+      `Command ${command} returned and unexpected response structure:`,
+      response,
+    );
+
+    throw new Error(`Command ${command} return an invalid or empty response.`);
+  } catch (e: any) {
+    // the AppError
+    if (checkAppError(e)) {
+      throw e;
+      // standard JS error
+    } else if (e instanceof Error) {
+      console.error(
+        `IPC or processing Error for command ${command}:`,
+        e.message,
+      );
+      // unknown error
+    } else {
+      console.error(`Unknown error during IPC call to ${command}:`, e);
+      throw new Error(`An unknown error occured while calling ${command}.`);
+    }
   }
+
+  throw new Error(
+    "invokeIpc failed to return or throw (this should never happen)",
+  );
 }
 
 export function cn(...inputs: ClassValue[]) {
